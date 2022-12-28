55a9d590cab2933ba0c6e2694b61dc093df0f517
vitu-77 <marcelo.victor05@gmail.com>
2022-12-28 16:36:25 -0300

    add: intranet iframe

--- src/components/IntranetLoginForm/index.tsx
+++ src/components/IntranetLoginForm/index.tsx
@@ -1,79 +1,17 @@
-import { Button } from '@components/Button'
-import { Input } from '@components/Input'
-import { useCallback, useState } from 'react'
-import { Container, Form, FormContainer, TextZone } from './stylex'
-
-type Errors = {
-	username: boolean
-	password: boolean
-}
+import { Container } from './stylex'
 
 export const IntranetLoginForm = () => {
-	const [username, setUsername] = useState('')
-	const [password, setPassword] = useState('')
-	const [errors, setErrors] = useState<Errors>({
-		password: false,
-		username: false,
-	})
-
-	const validateForm = useCallback((): Errors | null => {
-		const errors: Errors = {
-			username: !username,
-			password: !password,
-		}
-
-		return errors.password || errors.username ? errors : null
-	}, [username, password])
-
-	const handleSubmit = useCallback(() => {
-		const errors = validateForm()
-
-		if (errors) {
-			return setErrors(errors)
-		}
-
-		const data = { username, password }
-
-		console.log(data)
-	}, [validateForm, username, password, errors])
-
 	return (
 		<Container>
-			<FormContainer>
-				<TextZone>
-					<strong>Área de assinantes</strong>
-
-					<h1>Intranet</h1>
-
-					<p>Uso exclusivo das afiliadas Talk Radio.</p>
-				</TextZone>
-
-				<Form>
-					<Input
-						variant='yellow'
-						label='Nome de usuário'
-						placeholder='Digite seu usuário'
-						onChange={(value) => setUsername(value)}
-						style={{ marginBottom: '36px' }}
-					/>
-
-					<Input
-						variant='yellow'
-						type='password'
-						label='Senha'
-						placeholder='Digite sua senha'
-						onChange={(value) => setPassword(value)}
-						style={{ marginBottom: '36px' }}
-					/>
-
-					<Button
-						type='button'
-						content='Enviar'
-						variant='yellow'
-						onClick={handleSubmit}
-					/>
-				</Form>
-			</FormContainer>
+			<iframe
+				name='FRAME1'
+				src='https://intranet.talkradio.com.br/Default.aspx?ReturnUrl=%2f'
+				width='100%'
+				height='800'
+				marginHeight={0}
+				marginWidth={0}
+				scrolling='auto'
+				frameBorder={0}></iframe>
 		</Container>
 	)
 }
